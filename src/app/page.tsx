ba6d1adf49cb1c1a39188f28d47e0e9696d15420
StanislavShurko <stas.shurko@gmail.com>
2025-10-15 22:26:32 +0200

    Add input field

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,22 @@
-import Image from "next/image";
+"use client";
+import React from "react";
 import styles from "./page.module.css";
 
 export default function Home() {
+  const [question, setQuestion] = React.useState("");
+
   return (
-    <div>Hello World</div>
+    <div className={styles.mainContainer}>
+      <div>Ask question</div>
+      <div>
+        <input
+          type="text"
+          className={styles.input}
+          placeholder="Type here..."
+          value={question}
+          onChange={(e) => setQuestion(e.target.value)}
+        />
+      </div>
+    </div>
   );
 }
